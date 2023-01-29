dfe546edc351002a9e96a6d99669986ab99f1372
D-Sketon <2055272094@qq.com>
2023-01-29 11:39:50 +0800

    feat: make Loot Bag have slots(optional)

---
+++ src/main/java/cn/rainspace/lootbag/container/menu/LootBagMenu.java
@@ -0,0 +1,12 @@
+package cn.rainspace.lootbag.container.menu;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.MenuType;
+
+public class LootBagMenu extends ChestMenu {
+    public LootBagMenu(MenuType<?> p_39229_, int p_39230_, Inventory p_39231_, Container p_39232_, int p_39233_) {
+        super(p_39229_, p_39230_, p_39231_, p_39232_, p_39233_);
+    }
+}
--- src/main/java/cn/rainspace/lootbag/item/LootBagItem.java
+++ src/main/java/cn/rainspace/lootbag/item/LootBagItem.java
@@ -1,14 +1,21 @@
 package cn.rainspace.lootbag.item;
 
+import cn.rainspace.lootbag.block.entity.LootBagEntity;
 import cn.rainspace.lootbag.config.Config;
+import cn.rainspace.lootbag.container.menu.LootBagMenu;
+import cn.rainspace.lootbag.inventory.LootBagInventory;
 import cn.rainspace.lootbag.loot.ModLootTables;
+import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.StringTag;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -20,6 +27,7 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkHooks;
 
 import java.util.List;
 import java.util.Random;
@@ -32,10 +40,11 @@ public class LootBagItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         ItemStack itemstack = player.getItemInHand(hand);
-        if (!world.isClientSide) {
+        if (!world.isClientSide && hand == InteractionHand.MAIN_HAND) {
             LootTable table = world.getServer().getLootTables().get(ModLootTables.LOOT_BAG_GIFT);
             LootContext context = (new LootContext.Builder((ServerLevel) world)).withLuck(player.getLuck()).withParameter(LootContextParams.THIS_ENTITY, player).withParameter(LootContextParams.TOOL, itemstack).withParameter(LootContextParams.ORIGIN, player.position()).create(LootContextParamSets.GIFT);
             List<ItemStack> loot = table.getRandomItems(context);
+            NonNullList<ItemStack> filteredLoot = NonNullList.create();
             for (ItemStack itemStack : loot) {
                 boolean shouldGet = true;
                 for (String id : Config.BLACK_LIST.get()) {
@@ -45,6 +54,15 @@ public class LootBagItem extends Item {
                     }
                 }
                 if (shouldGet) {
+                    filteredLoot.add(itemStack);
+                }
+            }
+            if (Config.SLOT_MODE.get()) {
+                NetworkHooks.openGui((ServerPlayer) player, new LootBagEntity((id, playerInventory, unused) -> {
+                    return new LootBagMenu(MenuType.GENERIC_9x3, id, playerInventory, new LootBagInventory(filteredLoot), 3);
+                }, new TranslatableComponent("item.lootbag.loot_bag")));
+            } else {
+                for (ItemStack itemStack : filteredLoot) {
                     giveItem(player, itemStack);
                 }
             }
